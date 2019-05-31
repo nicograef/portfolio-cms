603d1e913b7d22232bea0b4f5527609e4dc592f6
nico <nico@urknall.co>
2019-05-31 23:01:15 +0200

    added cloud function to init user data when new account is created

---
+++ functions/index.js
@@ -0,0 +1,22 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions')
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin')
+admin.initializeApp()
+
+exports.initUserData = functions.auth.user().onCreate(user => {
+  const data = {
+    author: {
+      name: '',
+      profession: '',
+      bio: '',
+      image: 'https://portfolio-cms-0742.web.app/img/author.jpg'
+    },
+    items: {}
+  }
+  admin
+    .database()
+    .ref('/users/' + user.uid)
+    .push(data)
+})
